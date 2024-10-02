2d15d2bf7002cfe16e443b7a4e9c4871ba70713b
likhithashylu <likitha.shylu@gmail.com>
2024-10-02 18:37:34 +0530

    [LIKHITHA] add-Applying logic to check the attendance of an employee.

---
+++ CheckAttendance.py
@@ -0,0 +1,36 @@
+""" 
+
+    @Author: Likhitha S
+    @Date: 02-10-2024 18:00
+    @Last Modified by: Likhitha S
+    @Last Modified time: 02-10-2024 18:00
+    @Title:Write a Python program to check employee attendence using random.
+
+"""
+
+
+import random
+
+def check_attendance(attend):
+    """
+        
+        Description: 
+           This function is used to check employee attendence.
+        Parameters:
+          fun_name is used to an input from the user of type function.
+        Return:
+            It prints the output according to that fun_type . 
+        
+    """
+    
+    
+    
+    if attend==1:
+        print(attend," ->Employee is Present!!")
+    else:
+        print(attend," ->Employee is Absent!!") 
+    
+
+
+attend= random.randint(0,1)
+check_attendance(attend)
