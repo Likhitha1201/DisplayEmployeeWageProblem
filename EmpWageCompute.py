e9dc812b8db91f89b6c6a1ebe2f71d77e801ee9d
likhithashylu <likitha.shylu@gmail.com>
2024-10-03 10:04:40 +0530

    [LIKHITHA] add-Applying logic to calculate daily employee wage.

--- EmpWageCompute.py
+++ EmpWageCompute.py
@@ -11,7 +11,7 @@
 
 import random
 
-def employee_earnings(wage_earnsper_hour,working_hours):
+def employee_earnings():
     """
         
         Description: 
@@ -23,16 +23,11 @@ def employee_earnings(wage_earnsper_hour,working_hours):
         
     """
     
+    wage_earnsper_hour=20
+    working_hours=8
     
     total_earnings=wage_earnsper_hour*working_hours
     print(f"employee earning per day will be: {total_earnings} rupees")
   
     
 
-def main():
-    wage_earnsper_hour=20
-    working_hours=8
-    employee_earnings(wage_earnsper_hour,working_hours)
-
-if __name__=="__main__":
-    main()
